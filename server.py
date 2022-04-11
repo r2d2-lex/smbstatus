cd542dc26bf1b083758dba6dfeb91b14a690cc28
suser <r2d2-lex@yandex.ru>
2022-04-11 10:02:01 +0500

    небольшие проверки в возвр. значениях

--- server.py
+++ server.py
@@ -8,10 +8,20 @@ import re
 app = Flask(__name__)
 SKIP_LINES = 4
 UID = 1
+# Pid          Uid        DenyMode   Access      R/W        Oplock
+SIZE_LIST_OF_PART1 = 6
+# SharePath   Name   Time
+SIZE_LIST_OF_PART2 = 3
+PATH_TIME_LIST_SIZE = 2
 
 
 def parse_status(smb_data, uid_users_dict):
-    # 'Pid Uid DenyMode Access R_W Op_lock SharePath Name Time'
+    """
+    # 'Pid Uid DenyMode Access R_W Op_lock SharePath Name Time LoginName'
+    :param smb_data:
+    :param uid_users_dict:
+    :return:
+    """
     status_records = []
     count = 0
     for line in smb_data.splitlines():
@@ -34,7 +44,7 @@ def parse_status(smb_data, uid_users_dict):
     return status_records
 
 
-def parse_line(line):
+def parse_line(line) -> list:
     """
         Создаём список из параметров
     :param line:
@@ -45,22 +55,27 @@ def parse_line(line):
         part1 = split_line[0]
         part2 = split_line[1]
     except IndexError:
-        return ''
+        return []
 
     part1_list = parse_part1(part1)
+    if len(part1_list) < SIZE_LIST_OF_PART1:
+        print('Part1 bad size')
+        return []
+
     part2_list = parse_part2(part2)
-    if not part2_list:
-        return ''
+    if len(part2_list) < SIZE_LIST_OF_PART2:
+        print('Part2 bad size')
+        return []
 
     list_of_string = part1_list + part2_list
     return list_of_string
 
 
-def parse_part1(part1):
+def parse_part1(part1) -> list:
     return part1.split()
 
 
-def parse_part2(part2):
+def parse_part2(part2) -> list:
     """
         Возвращает список ['шара','путь к файлу','время доступа']
     :param part2:
@@ -71,28 +86,28 @@ def parse_part2(part2):
     try:
         string_split = part2.split(' ', 1)
     except ValueError:
-        return False
+        return []
 
     try:
         name_of_share = string_split[share_index]
         path_date_part = string_split[path_date_index]
     except IndexError:
-        return False
+        return []
 
     path_time_list = get_file_path_time(path_date_part)
-    if not path_time_list:
-        return False
+    if len(path_time_list) < PATH_TIME_LIST_SIZE:
+        return []
 
     return_part2_list = [name_of_share] + path_time_list
     return return_part2_list
 
 
-def get_file_path_time(path_time_part):
+def get_file_path_time(path_time_part) -> list:
     """
         Получаем имя файла и дату
         Ищем регуляркой: Пример: длиное_имя_файла.txt  Fri Apr  7 01:12:34 2022
     :param path_time_part:
-    :return: Список ['мия файла/папки', 'дата доступа']
+    :return: Список ['имя файла/папки', 'дата доступа']
     """
     need_tuple_size = 2
     path_time_part = path_time_part.strip()
@@ -100,17 +115,17 @@ def get_file_path_time(path_time_part):
     try:
         re_value_tuple = re_value[0]
     except IndexError:
-        return False
+        return []
 
     re_value_size = len(re_value_tuple)
     if re_value_size < need_tuple_size:
-        return False
+        return []
 
     try:
         path_of_file = re_value_tuple[0]
         time_of_access = re_value_tuple[1]
     except IndexError:
-        return False
+        return []
 
     path_of_file = path_of_file.rstrip()
     return [path_of_file, time_of_access]
